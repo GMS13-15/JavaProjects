3ef555221ded46a0d0e6a4fe0d3e37e79ea9397d
GMS2003 <saravanakumargm2003@gmail.com>
2022-06-01 20:40:31 +0530

    add

---
+++ newfile.py
@@ -0,0 +1,25 @@
+import turtle
+
+t = turtle.Turtle()
+s = turtle.Screen()
+s.bgcolor('black')
+t.hideturtle()
+t.goto(0,-10)
+
+t.pensize(3)
+t.color('red')
+t.begin_fill()
+t.left(140)
+t.forward(180)
+t.circle(-90,200)
+t.setheading(60)
+t.circle(-90,200)
+t.forward(170)
+t.end_fill()
+t.penup()
+t.goto(-75,130)
+t.pendown()
+t.color('white')
+t.write('GM Snegha',font=("verdana",11,"bold"))
+
+turtle.done()
